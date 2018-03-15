a5308117c7a50490378f48a1bf6984096fc5e0f3
Mark Sowell <markdismount@gmail.com>
2018-03-14 23:34:46 -0500

    Initial Commit 2
    adding py-pong.py

---
+++ py-pong.py
@@ -0,0 +1,49 @@
+import pygame
+
+# Defining colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+
+# Set the width and height of the screen [width, height)
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+# Title bar text
+pygame.display.set_caption("PyPong")
+
+# Main game loop conditional
+done = False
+
+# Screen update
+clock = pygame.time.Clock()
+
+# Main game loop
+while not done:
+    # main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # Game logic
+
+    # Clear the screen
+
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+
+    screen.fill(WHITE)
+
+    # Drawing code
+
+    # Update the screen with the draw
+    pygame.display.flip()
+
+    # Limit to 60 FPS
+    clock.tick(60)
+
+# Quit
+pygame.quit()
