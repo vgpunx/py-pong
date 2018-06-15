312adc2db88c99dc257b8db9dd32e61b923b5521
Mark Sowell <markdismount@gmail.com>
2018-06-14 23:44:50 -0500

    Re-commit of previous work

--- scoreboard.py
+++ scoreboard.py
@@ -13,15 +13,13 @@ class Scoreboard:
 
         self.position = (((DISPLAY_SIZE[0] / 2) - (self.size[0] / 2)), DISPLAY_SIZE[1] * 0.01)
 
-    def draw_zero(self):
-        # Draw a zero on the scoreboard
-        zero_list = [(20, 10), ((self.size[0] / 4), 10), ((self.size[0] / 4), (self.size[1] - 10)),
-                     (20, self.size[1] - 10), (20, 10)]
-        # lines(Surface, color, closed, pointlist, width=1)
-        pygame.draw.lines(self.image, pygame.Color('GREEN'), False, zero_list, 5)
-
     def draw(self, surface):
         self.image.fill(pygame.Color('WHITE'))
-        self.draw_zero()
 
         surface.blit(self.image, self.position)
+
+    def p1_point(self):
+        self.p1_score += 1
+
+    def p2_point(self):
+        self.p2_score += 1
---
+++ scoreboardnumeral.py
@@ -0,0 +1,74 @@
+import pygame
+
+class ScoreboardNumeral(pygame.sprite.Sprite):
+    def __init__(self, height, fg_color, bg_color, *groups, value):
+        """
+        :param height: int
+        :param fg_color: string
+        :param bg_color: string
+        :param groups: pygame.sprite.Group
+        :param value: int
+        """
+
+        super().__init__(*groups)
+
+        self.image = pygame.Surface((int(height / 2), height)).convert()
+        self.background = pygame.Surface((int(height / 2), height)).convert()
+
+        self.rect = self.image.get_rect()
+        self._layer = 0
+        self.size = height
+
+        self.fg_color = pygame.Color(fg_color)
+        self.bg_color = pygame.Color(bg_color)
+        self.value = value
+
+        self._digit_shapes = self._init_digit_shapes()
+        self._digit_strokewidth = int(self.size / 8)
+
+        self.background.fill(self.bg_color)
+
+    def set_value(self, value):
+        self.value = value
+        self. image.blit(self.background, (0, 0))
+        pygame.draw.lines(self.image, self.fg_color, False, self._digit_shapes[self.value], self._digit_strokewidth)
+
+    def get_value(self):
+        return self.value
+
+    def set_size(self, height):
+        self.size = height
+        self.image = pygame.Surface((int(self.size / 2), self.size)).convert()
+        self.background = pygame.Surface((int(self.size / 2), self.size)).convert()
+        self.rect = self.image.get_rect()
+        self.background.fill(self.bg_color)
+        self._digit_strokewidth = int(self.size / 8)
+        self._digit_shapes = self._init_digit_shapes()
+        self.set_value(self.value)
+
+    def get_size(self):
+        return self.rect.size
+
+    def _init_digit_shapes(self):
+        return (
+            (self.rect.topleft, self.rect.topright, self.rect.bottomright, self.rect.bottomleft, self.rect.topleft),
+            (self.rect.topright, self.rect.bottomright),
+            (self.rect.topleft, self.rect.topright, self.rect.midright, self.rect.midleft, self.rect.bottomleft,
+             self.rect.bottomright),
+            (self.rect.topleft, self.rect.topright, self.rect.midright, self.rect.midleft, self.rect.midright,
+             self.rect.bottomright, self.rect.bottomleft),
+            (self.rect.topleft, self.rect.midleft, self.rect.midright, self.rect.topright, self.rect.bottomright),
+            (self.rect.topright, self.rect.topleft, self.rect.midleft, self.rect.midright, self.rect.bottomright,
+             self.rect.bottomleft),
+            (self.rect.topleft, self.rect.bottomleft, self.rect.bottomright, self.rect.midright, self.rect.midleft),
+            (self.rect.topleft, self.rect.topright, self.rect.bottomright),
+            (self.rect.topleft, self.rect.topright, self.rect.bottomright, self.rect.bottomleft, self.rect.topleft,
+             self.rect.midleft, self.rect.midright),
+            (self.rect.midright, self.rect.midleft, self.rect.topleft, self.rect.topright, self.rect.bottomright)
+        )
+
+    def update(self):
+        super().update()
+
+    def draw(self, surface):
+        return surface.blit(self.image, self.rect.topleft)
